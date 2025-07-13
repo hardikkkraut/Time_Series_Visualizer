10cc1f423e605ef19cd72f70d282f8567aaa1bf1
Hardik Bhagvan Raut <rauthardik073@gmail.com>
2025-07-13 13:53:29 +0530

    Task 4 Uploaded

---
+++ main.py
@@ -0,0 +1,6 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Run all visualizations
+line_fig = draw_line_plot()
+bar_fig = draw_bar_plot()
+box_fig = draw_box_plot()
